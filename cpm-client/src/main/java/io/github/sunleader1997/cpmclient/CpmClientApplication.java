f54146f2938c8245ef62f3bec16cb1f23999112a
Sunleader <sunyaxing_1997@163.com>
2026-01-29 11:48:40 +0800

    init

--- cpm-client/src/main/java/io/github/sunleader1997/cpmclient/CpmClientApplication.java
+++ cpm-client/src/main/java/io/github/sunleader1997/cpmclient/CpmClientApplication.java
@@ -9,5 +9,4 @@ public class CpmClientApplication {
     public static void main(String[] args) {
         SpringApplication.run(CpmClientApplication.class, args);
     }
-
 }
--- cpm-server/src/main/java/io/github/sunleader1997/cpmserver/CpmServerApplication.java
+++ cpm-server/src/main/java/io/github/sunleader1997/cpmserver/CpmServerApplication.java
@@ -2,6 +2,7 @@ package io.github.sunleader1997.cpmserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
 public class CpmServerApplication {
